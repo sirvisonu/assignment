6fa87c120595dea418d972ebb4feaf104d473241
sirvisonu <snsivi1105@gmail.com>
2025-09-14 18:10:54 +0530

    first

---
+++ task3 q1.py
@@ -0,0 +1,21 @@
+#q1
+def factorial():
+    a=1
+    for i in range(1,n+1):
+        a=a*i
+    return a
+n=int(input("enter number: "))
+s=factorial()
+print("factorial of",n," is :",s)
+
+
+
+
+
+
+
+
+
+
+
+
